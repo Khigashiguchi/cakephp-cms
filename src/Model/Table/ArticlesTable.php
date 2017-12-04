95586d4a88f463d1ed46397374d7f067fb11424c
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-04 18:24:30 +0900

    Implement controller_and_templte add

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -2,11 +2,25 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config)
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+
+        // FIXME: 認証機構構築時に削除する
+        if (!$entity->user_id) {
+            $entity->user_id = 1;
+        }
     }
 }
